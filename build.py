9b9a2f56e7ef723395ee231e64ffc5f0dd7def57
Lutz Moppert <lutz@moppert.de>
2016-07-28 17:36:25 +0200

    Fixed bug in ignore file

--- build.py
+++ build.py
@@ -2,6 +2,12 @@ from staticjinja import make_site
 import glob
 
 
+def studio():
+    images = glob.glob('media/studio*.jpg')
+    images.sort()
+    return {'images': images}
+
+
 def frank():
     return {'images': glob.glob('media/Frank*.jpg')}
 
@@ -11,5 +17,7 @@ def nina():
 
 
 if __name__ == "__main__":
-    site = make_site(contexts=[('nina.html', nina), ('frank.html', frank), ])
+    site = make_site(contexts=[
+        ('nina.html', nina), ('frank.html', frank), ('studio.html', studio),
+    ])
     site.render(use_reloader=True)
